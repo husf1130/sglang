d69ba60559b1a1e5b038e2007ec8f81545ac6f80
husf1130 <hushoufeng@huawei.com>
2025-11-27 20:41:01 +0800

    Support L1+L3 distributed KVCache(memcache) on Ascend

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -153,6 +153,7 @@ from sglang.srt.managers.utils import GenerationBatchResult, validate_input_leng
 from sglang.srt.mem_cache.cache_init_params import CacheInitParams
 from sglang.srt.mem_cache.common import release_kv_cache
 from sglang.srt.mem_cache.radix_cache import RadixCache
+from sglang.srt.mem_cache.ascend_radix_cache import AscendHiRadixCache
 from sglang.srt.model_executor.forward_batch_info import ForwardMode
 from sglang.srt.multiplex.multiplexing_mixin import SchedulerMultiplexMixin
 from sglang.srt.parser.reasoning_parser import ReasoningParser
@@ -746,6 +747,25 @@ class Scheduler(
 
                 logger.info("Using experimental C++ radix tree implementation.")
                 self.tree_cache = RadixCacheCpp(params=params, server_args=server_args)
+            elif self.enable_hierarchical_cache and self.server_args.hicache_storage_backend == "memcache":
+                self.tree_cache = AscendHiRadixCache(
+                    req_to_token_pool=self.req_to_token_pool,
+                    token_to_kv_pool_allocator=self.token_to_kv_pool_allocator,
+                    tp_cache_group=(
+                        self.attn_tp_cpu_group
+                        if self.server_args.enable_dp_attention
+                        else self.tp_cpu_group
+                    ),
+                    page_size=self.page_size,
+                    eviction_policy=server_args.radix_eviction_policy,
+                    enable_metrics=self.enable_metrics,
+                    hicache_storage_backend=server_args.hicache_storage_backend,
+                    is_eagle=self.spec_algorithm.is_eagle(),
+                    device_id=self.gpu_id,
+                )
+                self.tp_worker.register_hicache_layer_transfer_counter(
+                    self.tree_cache.cache_controller.layer_done_counter
+                )
             elif self.enable_hierarchical_cache:
                 from sglang.srt.mem_cache.hiradix_cache import HiRadixCache
 
@@ -1435,7 +1455,7 @@ class Scheduler(
     def _prefetch_kvcache(self, req: Req):
         if self.enable_hicache_storage:
             req.init_next_round_input(self.tree_cache)
-            if req.last_node.backuped:
+            if req.last_node != self.tree_cache.root_node and req.last_node.backuped:
                 # only to initiate the prefetch if the last node is backuped
                 # otherwise, the allocated GPU memory must be locked for integrity
                 last_hash = req.last_host_node.get_last_hash_value()
@@ -1784,6 +1804,7 @@ class Scheduler(
                 req,
                 has_chunked_req=(self.chunked_req is not None),
                 truncation_align_size=self.truncation_align_size,
+                enable_hierarchical_cache=self.enable_hierarchical_cache,
             )
 
             if res != AddReqResult.CONTINUE:
